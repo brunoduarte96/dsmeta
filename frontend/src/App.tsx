7a6a57f04ee186b588b1e6a7bd4b3e3f3391cbf1
Bruno Duarte <brunoduarte96@outlook.com>
2022-11-20 13:10:40 -0300

    Other Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,23 @@
-import NotificationButton from "./components/NotificationButton"
+
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
 
 
 function App() {
-return(
-  <>
-  <h1>Ola mundo</h1>
-  <NotificationButton/>
-  </>
-)
+  return (
+    <>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+          <SalesCard/>
+
+
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
